210401dddce6273724b8ce582d4dd1804cce4558
msuhaibk <suhaibmohd98@gmail.com>
2024-06-12 14:40:58 +0400

    retraced

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import GraphiQLPlayground from './components/GraphiQLPlayground';
 
 const Home: React.FC = () => (
   <div style={{ height: '100vh' }}>
-    {/* <GraphiQLPlayground /> */}
-    HELLOW TEST NEXT
+    <GraphiQLPlayground />
+    {/* HELLOW TEST NEXT */}
   </div>
 );
 
