584ad4d9fd6fb3eb7fb44078d3c31e45f8cc56d8
msuhaibk <suhaibmohd98@gmail.com>
2024-06-12 14:49:48 +0400

    Added graphql-ws dynamic import playground component [window undefined err]

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,18 @@
 import React from 'react';
-import GraphiQLPlayground from './components/GraphiQLPlayground';
+// import GraphiQLPlayground from './components/GraphiQLPlayground';
+import dynamic from 'next/dynamic';
 
-const Home: React.FC = () => (
-  <div style={{ height: '100vh' }}>
-    <GraphiQLPlayground />
-    {/* HELLOW TEST NEXT */}
-  </div>
-);
+const GraphiQLPlayground = dynamic(() => import('./components/GraphiQLPlayground'), {
+  ssr: false
+});
+
+const Home: React.FC = () => {
+  const isBrowser = typeof window !== 'undefined';
+  return (
+    <div style={{ height: '100vh' }}>
+      {isBrowser && <GraphiQLPlayground />}
+    </div>
+  );
+};
 
 export default Home;
